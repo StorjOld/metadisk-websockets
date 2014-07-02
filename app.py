06993105b2499fe561853587990ecbfc2aa68798
Josh Brandoff <josh@joshua.is>
2014-07-02 01:48:50 -0400

    Added additional information to client connect/disconnect printouts

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from gevent import monkey
 monkey.patch_all()
 
 import time
+from datetime import datetime
 from threading import Thread
 import settings
 import requests
-from flask import Flask
+from flask import Flask, request
 from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -38,11 +39,11 @@ def metadisk_connect():
 	if thread is None:
 		thread = Thread(target=status_thread)
 		thread.start()
-	print('Client has connected.')
+	print('Client has connected from ' + request.remote_addr + ' at ' + str(datetime.now()))
 
 @socketio.on('disconnect', namespace='/metadisk')
 def metadisk_disconnect():
-    print('Client has disconnected.')
+    print('Client has disconnected from ' + request.remote_addr + ' at ' + str(datetime.now()))
 
 if __name__ == '__main__':
 	socketio.run(app)
